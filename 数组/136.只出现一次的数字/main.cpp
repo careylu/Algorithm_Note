cc05cd47a424287690cdc352d9b15a50e81d98bc
careylu <careylu@tencent.com>
2023-10-30 19:42:52 +0800

    136.只出现一次的数字提交

--- 数组/136.只出现一次的数字/main.cpp
+++ 数组/136.只出现一次的数字/main.cpp
@@ -1,9 +1,37 @@
 // Copyright 2023, Tencent Inc.
 // Author: careylu
 // Date: 10/29/23
-#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include<limits.h>
+using namespace std;
+
+int singleNumber(vector<int>& nums)
+{
+    int res = 0;
+    for (int num : nums)
+    {
+        res ^= num;
+    }
+    return res;
+}
 
 int main()
 {
-    printf("hello world\n");
+    vector<int> nums = {1, 2, 1, 2, 3, 4, 4};
+    printf("res: %d", singleNumber(nums));
+    return 0;
 }
